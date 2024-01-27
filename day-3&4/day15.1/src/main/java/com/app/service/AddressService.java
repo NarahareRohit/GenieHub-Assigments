2424bc426bdf45c464821a1ff8cd361e74be6ed7
Rohit Narahare <71591573+NarahareRohit@users.noreply.github.com>
2024-01-27 15:54:47 +0530

    Delete day-3&4/day15.1 directory

--- day-3&4/day15.1/src/main/java/com/app/service/AddressService.java
+++
@@ -1,8 +0,0 @@
-package com.app.service;
-
-import com.app.pojos.Address;
-
-public interface AddressService {
-//add a method to get address details by id
-	Address getAddressDetails(long adrId);
-}
